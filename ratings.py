2217885ae123d3be529a17fb1eeaf439a07ee778
Emily Chen <emchen.c@gmail.com>
2022-02-07 02:30:40 +0000

    lab complete

--- ratings.py
+++ ratings.py
@@ -18,3 +18,12 @@ for line in file:
 #Spit out ratings in alphabetical order
 for restaurant, rating in sorted(ratings.items()):
     print(f"{restaurant} is rated at {rating}.")
+
+def add_ratings():
+    """Prompts user for a new restaurant and rating"""
+    new_restaurant = input("Restaurant name? ")
+    new_score = input("Restaurant score? ")
+    ratings[new_restaurant] = new_score
+
+    for restaurant, rating in sorted(ratings.items()):
+        print(f"{restaurant} is rated at {rating}.")
